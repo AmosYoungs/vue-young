e9e5fff4be49c4e375a6a41d8bf0fa4d9782cab7
yanggl <ygl_mail@163.com>
2023-11-18 15:17:47 +0800

    订单管理页面更新

--- src/views/privateWork/HaiBaChiOrder.vue
+++ src/views/privateWork/HaiBaChiOrder.vue
@@ -1,25 +1,41 @@
 <template>
   <div class="flex-ver">
-    <div class="app-container ">
+    <div class="app-container">
       <div class="header-title">Hibachi Order Form</div>
       <div class="base-info">
-        <div class="flex">
+      <el-row  >
+        
+        <el-col :span="12" class="flex">
           <span class="label">Booking Name：</span>
           <el-input class="flex-1" v-model="order.bookingName"></el-input>
-        </div>
+        </el-col>
 
-        <div class="flex">
+        <el-col :span="12" class="flex">
           <span class="label">Booking Address：</span>
           <el-input class="flex-1" v-model="order.bookingAddress"></el-input>
-        </div>
+        </el-col>
+      </el-row>
+      <el-row style="margin:10px 0">
+        <el-col :span="24" class="flex">
+          <span class="label">Booking Date</span>
+          <el-date-picker
+            v-model="order.bookingDate"
+            type="datetime"
+            style="width:95%"
+            value-format="yyyy-MM-dd HH:mm"
+            format="yyyy-MM-dd HH:mm"
+            prefix-icon=""
+          >
+          </el-date-picker>
+        </el-col>
+      </el-row>
       </div>
-
       <!-- 表格 -->
-     
       <el-table
         :data="order.orderDetail"
         class="el-table"
         border
+        fit
         height="75%"
         show-summary
         :summary-method="getSummaries"
@@ -30,166 +46,50 @@
           align="center"
           :width="flexColumnWidth('No.', 'No.')"
         ></el-table-column>
-        <el-table-column
-          prop="name"
-          label="name"
-          align="center"
-          :width="flexColumnWidth('name', 'name')"
-        >
-          <template slot-scope="scope">
-            <el-input v-model="scope.row.name"> </el-input>
-          </template>
-        </el-table-column>
-
-        <el-table-column
-          class="col-class"
-          prop="F0001"
-          align="center"
-          :label="menus.F0001"
-          :width="flexColumnWidth(menus.F0001, 'Veggie')"
-        >
-          <template slot-scope="scope">
-            <el-input-number
-              size="large"
-              :min="0"
-              @onblur="blurNum"
-              v-model="scope.row.F0001"
-            >
+       
+        <el-table-column v-for="column in tablColumns" :prop="column.valuekey" :key="column.label" :label="column.label" :width="flexColumnWidth(column.valuekey, column.label)">
+          <template  slot-scope="scope" >
+            <el-input v-if="column.type=='input'" v-model="scope.row[column.valuekey]"> </el-input>
+            <el-input v-if="column.type=='textarea'" type="textarea" v-model="scope.row[column.valuekey]"> </el-input>
+           <el-input-number v-else-if="column.type=='number'" size="small" :min="0" v-model="scope.row[column.valuekey]">
             </el-input-number>
           </template>
-        </el-table-column>
-
-        <el-table-column
-          prop="F0002"
-          :label="menus.F0002"
-          align="center"
-          :width="flexColumnWidth(menus.F0002, 'Chicken')"
-        >
-          <template slot-scope="scope">
-            <el-input-number size="large" :min="0" v-model="scope.row.F0002">
-            </el-input-number>
-          </template>
-        </el-table-column>
-
-        <el-table-column
-          prop="F0003"
-          :label="menus.F0003"
-          align="center"
-          :width="flexColumnWidth(menus.F0003, 'Shrimp')"
-        >
-          <template slot-scope="scope">
-            <el-input-number size="large" :min="0" v-model="scope.row.F0003">
-            </el-input-number>
-          </template>
-        </el-table-column>
-
-        <el-table-column
-          prop="F0004"
-          :label="menus.F0004"
-          align="center"
-          :width="flexColumnWidth(menus.F0004, 'Steak')"
-        >
-          <template slot-scope="scope">
-            <el-input-number size="large" ::min="0" v-model="scope.row.F0004">
-            </el-input-number>
-          </template>
-        </el-table-column>
-
-        <el-table-column
-          prop="F0005"
-          :label="menus.F0005"
-          align="center"
-          :width="flexColumnWidth(menus.F0005, 'Salmon')"
-        >
-          <template slot-scope="scope">
-            <el-input-number size="large" :min="0" v-model="scope.row.F0005">
-            </el-input-number>
-          </template>
-        </el-table-column>
-
-        <el-table-column
-          prop="F0006"
-          :label="menus.F0006"
-          align="center"
-          :width="flexColumnWidth(menus.F0006, 'Scallops')"
-        >
-          <template slot-scope="scope">
-            <el-input-number size="large" :min="0" v-model="scope.row.F0006">
-            </el-input-number>
-          </template>
-        </el-table-column>
-
-        <el-table-column
-          prop="F0007"
-          :label="menus.F0007"
-          align="center"
-          :width="flexColumnWidth(menus.F0007, 'Filet +$8')"
-        >
-          <template slot-scope="scope">
-            <el-input-number size="large" :min="0" v-model="scope.row.F0007">
-            </el-input-number>
-          </template>
-        </el-table-column>
-
-        <el-table-column
-          prop="F0008"
-          :label="menus.F0008"
-          align="center"
-          :width="flexColumnWidth(menus.F0008, 'Lobs +$15')"
-        >
-          <template slot-scope="scope">
-            <el-input-number size="large" :min="0" v-model="scope.row.F0008">
-            </el-input-number>
-          </template>
-        </el-table-column>
-
-        <el-table-column
-          prop="Allergies"
-          label="Allergies"
-          align="center"
-          :width="flexColumnWidth('Allergies', 'Allergies')"
-        >
-          <template slot-scope="scope">
-            <el-input type="textarea" v-model="scope.row.Allergies"> </el-input>
-          </template>
-        </el-table-column>
-
+      </el-table-column>
         <el-table-column
           label="操作"
           align="center"
-          :width="flexColumnWidth(menus.F0007, 'Filet +$8')"
+          width="110px"
         >
           <template slot-scope="scope">
-              <el-button
-                class="operate-button"
-                icon="el-icon-refresh-right"
-                size="small"
-                @click="resetRow(scope.row)"
-                >reset</el-button
-              >
-              <el-button
+            <el-button
+              class="operate-button"
+              icon="el-icon-refresh-right"
+              size="small"
+              @click="resetRow(scope.row)"
+              >reset</el-button
+            >
+            <el-button
               type="danger"
-                class="operate-button"
-                icon="el-icon-delete"
-                size="small"
-                @click="deleteRow(scope.row)"
-                >delete</el-button
-              >
+              class="operate-button"
+              icon="el-icon-delete"
+              size="small"
+              @click="deleteRow(scope.row)"
+              >delete</el-button
+            >
           </template>
         </el-table-column>
       </el-table>
-     
     </div>
     <div class="footer-div">
       <el-button
-          type="primary"
-          class="buttonAdd"
-          icon="el-icon-plus"
-          size="small"
-          plain
-          @click="addRow"
-          >add row</el-button
-        >
+        type="primary"
+        class="buttonAdd"
+        icon="el-icon-plus"
+        size="small"
+        plain
+        @click="addRow"
+        >add row</el-button
+      >
       <el-button
         type="primary"
         size="small"
@@ -207,48 +107,72 @@ name = "MainVue";
 export default {
   data() {
     return {
-      tableData: [],
-      menus: {
-        F0001: "Veggie",
-        F0002: "Chicken",
-        F0003: "Shrimp",
-        F0004: "Steak",
-        F0005: "Salmon",
-        F0006: "Scallops",
-        F0007: "Filet +$8",
-        F0008: "Lobs +$15",
+      tablColumns:[
+      {
+        label:'name',
+        valuekey:'name',
+        type:'input',
       },
+      {
+        label:'Veggie',
+        valuekey:'F0001',
+        type:'number',
+      },
+      {
+        label:'Chicken',
+        valuekey:'F0002',
+        type:'number',
+      },
+      {
+        label:'Shrimp',
+        valuekey:'F0003',
+        type:'number',
+      },
+      {
+        label:'Steak',
+        valuekey:'F0004',
+        type:'number',
+      },
+        {
+        label:'Salmon',
+        valuekey:'F0005',
+        type:'number',
+      },
+      {
+        label:'Scallops',
+        valuekey:'F0006',
+        type:'number',
+      },
+      {
+        label:'Filet +$8',
+        valuekey:'F0007',
+        type:'number',
+      },
+      {
+        label:'Lobs +$15',
+        valuekey:'F0008',
+        type:'number',
+      },
+      {
+        label:'Allergies',
+        valuekey:'Allergies',
+        type:'textarea',
+      }
+      ],
       order: {
+        bookingDate: "",
         bookingName: "",
         bookingAddress: "",
-        orderDetail: [
-          {
-            name: "",
-            F0001: 0,
-            F0002: 0,
-            F0003: 0,
-            F0004: 0,
-            F0005: 0,
-            F0006: 0,
-            F0007: 0,
-            F0008: 0,
-            Allergies: "",
-          },
-        ],
-      },
-
-      total: 0,
-      newRow: {}, // 新增的一行数据
-      isDis: false,
-      fullHeight: window.innerHeight - 220 + "px",
+        orderDetail: [],
+      }, 
     };
   },
-  mounted() {},
+  created() {this.requestTableColumn()},
   methods: {
-    // getSummary(params){
-    //   console.log(params)
-    //
-    // },
+   requestTableColumn(){
+    // TODO 请求获取tablColumns
+    this.order.orderDetail.push(this.initRow())
+   },
     getSummaries(param) {
       const { columns, data } = param;
       const sums = [];
@@ -293,9 +217,9 @@ export default {
       const arr = this.order.orderDetail.map((x) => x[prop]);
       if (arr.length > 0) {
         arr.push(label);
-        return this.getMaxLength(arr) + 160 + "px";
+        return this.getMaxLength(arr) + 100 + "px";
       }
-      return 160 + "px";
+      return 100 + "px";
     },
     /**计算列内容最大宽度
      * 遍历列的所有内容，获取最宽一列的宽度
@@ -327,35 +251,16 @@ export default {
       return width;
     },
 
-    blurNum(value) {
-      console.log(value);
-    },
-    initRow(row) {
-      if (row == null || row == "" || row == undefined) {
-        const newRow = {
-          name: "",
-          F0001: 0,
-          F0002: 0,
-          F0003: 0,
-          F0004: 0,
-          F0005: 0,
-          F0006: 0,
-          F0007: 0,
-          F0008: 0,
-          Allergies: "",
-        };
+    initRow() {
+      const newRow = {}
+      this.tablColumns.forEach(column=>{
+        if(column.type==='number'){
+          newRow[column.valuekey] =0
+        }else{
+          newRow[column.valuekey] =''
+        }
+      })
         return newRow;
-      }
-      row.name = "";
-      row.F0001 = "";
-      row.F0002 = "";
-      row.F0003 = "";
-      row.F0004 = "";
-      row.F0005 = "";
-      row.F0006 = "";
-      row.F0007 = "";
-      row.F0008 = "";
-      row.Allergies = "";
     },
     addRow() {
       // 添加一行数据
@@ -363,7 +268,14 @@ export default {
       this.order.orderDetail.push(newRow);
     },
     resetRow(row) {
-      row = this.initRow(row);
+      Object.keys(row).forEach(key=>{
+        console.log(key,key.indexOf('000'))
+        if(key.indexOf('000')>-1){
+          row[key] = 0
+        }else{
+          row[key] = ''
+        }
+        })
     },
     deleteRow(row) {
       console.log(row);
@@ -374,7 +286,9 @@ export default {
         this.order.orderDetail.splice(index, 1);
       }
     },
-    submitForm() {},
+    submitForm() {
+      console.log(this.order)
+    },
   },
 };
 </script>
@@ -385,7 +299,7 @@ body {
   width: 100vw;
   height: 100vh;
 }
-.flex-ver{
+.flex-ver {
   display: flex;
   flex-direction: column;
 }
@@ -393,9 +307,9 @@ body {
   flex: 1;
   padding: 0 5px;
 }
-.scroll-container{
-flex: 1;
-overflow: scroll;
+.scroll-container {
+  flex: 1;
+  overflow: scroll;
 }
 .header-title {
   font-size: 24px;
@@ -403,11 +317,11 @@ overflow: scroll;
   height: 35px;
   line-height: 35px;
 }
-/* .el-table {
-  margin-bottom: 6%;
-  border-width: medium;
-  font-size: 30px;
-} */
+.el-table {
+  margin-top: 10px;
+  /* border-width: medium;
+  font-size: 30px; */
+}
 .el-table .cell {
   white-space: normal;
   word-break: break-all;
@@ -428,23 +342,22 @@ el-table-column {
   /* bottom: 0px; */
   width: 100%;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   height: 50px;
   padding: 5px;
 }
 .el-input__inner {
-  font-size: 30px;
+  /* font-size: 30px; */
   line-height: 20em;
 }
 
-
 .buttonSubmit {
-  width: 45%;
+  width: 40%;
 }
-.buttonAdd{
-  width: 45%;
+.buttonAdd {
+  width: 40%;
 }
-.operate-button{
+.operate-button {
   margin: 0 5px;
 }
 .el-textarea__inner {
@@ -466,5 +379,4 @@ el-table-column {
   font-size: 14px;
   width: 80px;
 }
-
 </style>
